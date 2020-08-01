251b9f05ba13bf2458bc9b9a813acf161ca02aca
sir-maniac <608494+sir-maniac@users.noreply.github.com>
2020-08-01 08:13:24 -0600

    Remove VERSION constant.
    The jar manifest makes this variable redundant.

--- src/main/java/com/dizzyd/creativezone/CreativeZoneMod.java
+++ src/main/java/com/dizzyd/creativezone/CreativeZoneMod.java
@@ -40,7 +40,6 @@ import java.util.HashSet;
 @Mod(CreativeZoneMod.MODID)
 public class CreativeZoneMod {
     public static final String MODID = "creativezone";
-    public static final String VERSION = "1.0.2";
 
     private static final Logger LOGGER = LogManager.getLogger();
 
